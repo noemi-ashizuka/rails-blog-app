338ac78a80acfb1e8a45879e0d94f2e451e6d47a
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-09-07 16:54:18 +0900

    added basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+
+Article.destroy_all
+User.destroy_all
+
+puts "creating Users"
+
+joe = User.create!(
+  email: "joe@example.com",
+  password: "123456"
+)
+
+sylvia = User.create!(
+  email: "sylvia@example.com",
+  password: "123456"
+)
+
+puts "Users created"
+
+puts "creating articles"
+
+article_one = Article.create!(
+  title: "Article One",
+  content: "Quisque at pretium felis. Morbi nec consectetur odio. Integer et vehicula lorem.",
+  date: Date.new(2020, 8, 12),
+  is_published: true,
+  user: joe
+)
+
+article_two = Article.create!(
+  title: "Article Two",
+  content: "Nam commodo ultrices nisl, a dapibus tellus mattis sit amet. Cras tincidunt est a est tempor tristique non at massa.",
+  date: Date.new(2020, 6, 19),
+  is_published: true,
+  user: sylvia
+)
+
+article_three = Article.create!(
+  title: "Article Three",
+  content: "Sed venenatis auctor egestas. Sed tempor eu erat id tincidunt.",
+  date: Date.new(2020, 8, 5),
+  is_published: false,
+  user: sylvia
+)
+
+article_four = Article.create!(
+  title: "Article Four",
+  content: "Quisque vehicula nunc non magna pellentesque mollis. Proin ut congue ipsum, et tempus justo.",
+  date: Date.new(2020, 7, 15),
+  is_published: false,
+  user: joe
+)
+
+puts "articles created"
