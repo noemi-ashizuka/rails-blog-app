b89c470b60e5f34b21f7b43b99480258e2f3169a
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-09-07 19:13:37 +0900

    created view for new article

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: "articles#index"
+  
+  resources :articles, only: :new
 end
---
+++ spec/features/user_creates_new_article_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+feature "User creates new article" do
+  scenario "successfully" do
+    visit root_url
+
+    click_on "New"
+    fill_in "Title", with: "New Article"
+    fill_in "Content", with: "Lorem ipsum something"
+    fill_in "Date", with: "2020-8-30"
+    click_on "Save"
+  end
+end
