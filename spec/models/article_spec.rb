199255fc6210e227e5721f2f51dea8cef7c61081
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-09-07 11:50:42 +0900

    added basic tests for article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @user_one = build(:user)
+  end
+
+  subject {
+    described_class.new(title: "My awesome article",
+                        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        date: Date.new(2020, 7, 24),
+                        user: @user_one)
+  }
+
+  before { subject.save }
+
+  it 'should be valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+  it 'should not valid without a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+  it 'should not valid without a content' do
+    subject.content = nil
+    expect(subject).to_not be_valid
+  end
+  it 'should not valid without a date' do
+    subject.date = nil
+    expect(subject).to_not be_valid
+  end
+  it 'should not valid without a user' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
 end
