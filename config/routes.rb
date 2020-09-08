673b3a462b8e245a2b0bde234baf8416da3e28d8
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-09-08 15:51:45 +0900

    added destroy action for article

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   
   root to: "articles#index"
   
-  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  resources :articles
 end
---
+++ spec/features/user_deletes_article_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+feature "User deletes new article" do
+  before(:each) do
+    @user = create(:user, email: "mimi@example.com", password: "123456")
+    @article_to_delete = create(:article, title: "Random article", content: "Some content in here", date: "2020/3/18", user: @user)
+  end
+
+  scenario "successfully" do
+    visit root_url
+    sign_in(@user)
+    click_on "Delete"
+    
+    expect(Article.count).to eq(0)
+  end
+end
