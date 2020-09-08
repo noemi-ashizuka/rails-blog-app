a02763b46478bba474465a3a59e9233638f80736
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-09-08 14:23:47 +0900

    added show action and view for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
+    email { "joe@example.com" }
+    password { "123456" }
+  end
+
+  factory :random_user do
     email { Faker::Internet.safe_email }
     password { Faker::Internet.password }
   end
