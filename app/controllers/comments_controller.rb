d9cbdd9bb994c0bfba7d2cab218c76091d35de73
Noemi Ashizuka <noemi.calanducci@gmail.com>
2021-02-17 16:15:30 +0900

    show comments and form in article show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,23 @@ class CommentsController < ApplicationController
     @comment = Comment.new
     authorize @comment
   end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @article = Article.find(params[:article_id])
+    @comment.article = @article
+    @comment.user = current_user
+    authorize @comment
+    if @comment.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
